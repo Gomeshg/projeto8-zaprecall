74efe2fa6772f69084eb1fdd56ac19d9b7dcc097
Hugo Gomes <gomes.hugo00@gmail.com>
2022-07-20 13:55:13 -0300

    Tela Inicial

---
+++ src/Components/Welcome.js
@@ -0,0 +1,10 @@
+export default function Welcome(){
+    return (
+        <div className="welcome">
+            <img src="./image/logo-grande.png" alt="Zap Logo" />
+            {/* <div className="logo-recall-welcome"></div> */}
+            <h1>ZapRecall</h1>
+            <button className="btn-init-recall">Iniciar Recall!</button>
+        </div>
+    );
+}
